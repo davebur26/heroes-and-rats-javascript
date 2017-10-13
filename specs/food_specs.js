08baac087560b3a32d735113703433579caf0136
davebur26 <davebur26@gmail.com>
2017-10-13 16:16:41 +0100

    add test setup and name test

---
+++ specs/food_specs.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+var Food = require('../food.js')
+
+
+describe('Food', function(){
+  var food;
+
+  beforeEach(function () {
+    food = new Food("Cabbage", 15);
+  })
+
+  it("Should get food's name", function(){
+    assert.strictEqual("Cabbage", food.name)
+  })
+
+
+
+
+})
