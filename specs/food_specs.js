28b81dad8cac276688c1500ef645e8b683cc0abe
davebur26 <davebur26@gmail.com>
2017-10-14 21:49:50 +0100

    add rat poison test

--- specs/food_specs.js
+++ specs/food_specs.js
@@ -1,12 +1,15 @@
 var assert = require('assert')
 var Food = require('../food.js')
+var Rat = require('../rats.js')
 
 
 describe('Food', function(){
   var food;
+  var rat;
 
   beforeEach(function () {
     food = new Food("Cabbage", 15);
+    rat = new Rat();
   })
 
   it("Should get food's name", function(){
@@ -15,6 +18,11 @@ describe('Food', function(){
 
   it("Should get food's calories", function(){
     assert.strictEqual(15, food.calories)
+  })
+
+  it("rat should poison food when touched", function(){
+    rat.touch(food)
+    assert.strictEqual(true, food.poisoned)
   })
 
 
